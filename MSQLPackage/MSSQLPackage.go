d715a3a27b83eb8502aaae942308afdc41ec9d2a
Yash Wagle <ywagle@tibco.com>
2018-05-12 01:47:13 +0530

    Final commit

--- MSQLPackage/MSSQLPackage.go
+++ MSQLPackage/MSSQLPackage.go
@@ -1,10 +1,11 @@
 package MSQLPackage
 
 import (
-"database/sql"
-"fmt"
-"strings"
- _"github.com/denisenkom/go-mssqldb"
+	"database/sql"
+	"fmt"
+	"strings"
+
+	_ "github.com/denisenkom/go-mssqldb"
 )
 
 func UpdateQuery(username string, password string, host string, port string, dbname string, query string) (string, error) {
@@ -35,8 +36,8 @@ func UpdateQuery(username string, password string, host string, port string, dbn
 	op := `{"numberOfRowsAffected":"` + fmt.Sprintf("%v", num) + `"}` //Getting number of Rows Affected
 	return op, nil
 }
-func FireQuery(username string, password string, host string, port string, dbname string, query string) (string,error){
-	dsn := "server=" + host + ";user id=" + username + ";password=" + password + ";port="+port+";database=" + dbname 								//constructing the URL
+func FireQuery(username string, password string, host string, port string, dbname string, query string) (string, error) {
+	dsn := "server=" + host + ";user id=" + username + ";password=" + password + ";port=" + port + ";database=" + dbname //constructing the URL
 	db, err := sql.Open("mssql", dsn)
 	if err != nil {
 		//fmt.Println("Cannot connect: ", err.Error())														//Cannot connect to DB
@@ -49,65 +50,88 @@ func FireQuery(username string, password string, host string, port string, dbnam
 	}
 	defer db.Close()
 	var result string
- result,err = exec(db, query)																									//Calling the execute function
-		if err != nil {
-			//fmt.Println(err)
-			return "Some error ",err
-		}
-		return result,nil
+	result, err = exec(db, query) //Calling the execute function
+	if err != nil {
+		//fmt.Println(err)
+		return "Some error ", err
+	}
+	return result, nil
 }
 
-func exec(db *sql.DB, cmd string) (result string,err error) {
-	var op string
-rows, err := db.Query(cmd)																										//Executing the query
+func CreateQuery(username string, password string, host string, port string, dbname string, query string) (string, error) {
+	dsn := "server=" + host + ";user id=" + username + ";password=" + password + ";port=" + port + ";database=" + dbname //constructing the URL
+	db, err := sql.Open("mssql", dsn)
 	if err != nil {
-		return "Erro executing query",err
+		fmt.Println("Cannot connect: ", err.Error()) //Cannot connect to DB
+		return "Cannot connect: ", err
+	}
+	err = db.Ping()
+	if err != nil {
+		fmt.Println("Cannot connect: ", err.Error()) //Cannot Connect to DB
+		return "Cannot connect: ", err
+	}
+	defer db.Close()
+
+	_, err = db.Exec(query)
+	if err != nil {
+		return "Error executing query", err
+	}
+
+	op := `"Query Status":"Operation Successful"` //Getting The row
+	return op, nil
+
+}
+
+func exec(db *sql.DB, cmd string) (result string, err error) {
+	var op string
+	rows, err := db.Query(cmd) //Executing the query
+	if err != nil {
+		return "Erro executing query", err
 	}
 	defer rows.Close()
-	cols, err := rows.Columns()																										//Getting the columns
+	cols, err := rows.Columns() //Getting the columns
 	if err != nil {
-		return "",err
+		return "", err
 	}
 	if cols == nil {
-		return "No Columns",nil
+		return "No Columns", nil
 	}
 
 	vals := make([]interface{}, len(cols))
-	for i := 0; i < len(cols); i++ {																			//making an interface to store the row values
+	for i := 0; i < len(cols); i++ { //making an interface to store the row values
 		vals[i] = new(interface{})
-
 
 	}
 
-		var currRow string;
-		var str string;
-		op=`{ "rows": [`
-		var currEle string
-	for rows.Next() {																									//iterating through the rows
+	var currRow string
+	var str string
+	op = `{ "rows": [`
+	var currEle string
+	for rows.Next() { //iterating through the rows
 
-		currRow="";
-		err = rows.Scan(vals...)																					//getting one row
+		currRow = ""
+		err = rows.Scan(vals...) //getting one row
 		if err != nil {
 			continue
 		}
-		for i := 0; i < len(vals); i++ 		{																//iterating through columns
+		for i := 0; i < len(vals); i++ { //iterating through columns
 			if i != 0 {
 				fmt.Print("\t")
 			}
-				v:=(vals[i].(*interface{}))
-				str=fmt.Sprintf("%v",(*v))
-				currEle=`{"column":{"name":"`+cols[i]+`",`+`"value":"`+ str +`"}}`
-			currRow=currRow+currEle+","
+			v := (vals[i].(*interface{}))
+			str = fmt.Sprintf("%v", (*v))
+			currEle = `{"column":{"name":"` + cols[i] + `",` + `"value":"` + str + `"}}`
+			currRow = currRow + currEle + ","
 		}
-		currRow=strings.TrimSuffix(currRow,`,`)
-		op=op+`{"row":[`+currRow+`]},`
+		currRow = strings.TrimSuffix(currRow, `,`)
+		op = op + `{"row":[` + currRow + `]},`
 
-}
-	if rows.Err() != nil {
-		return "Some error",rows.Err()
 	}
-	op=strings.TrimSuffix(op,`,`)
-	op=op+`]}`
+	if rows.Err() != nil {
+		return "Some error", rows.Err()
+	}
+	op = strings.TrimSuffix(op, `,`)
+	op = op + `]}`
 
-	return op,nil
+	return op, nil
 }
