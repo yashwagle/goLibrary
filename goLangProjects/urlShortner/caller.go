286fef84def292004b52084bb6877c58a7c2a92b
Yash Wagle <yash27wagle@gmail.com>
2018-08-19 19:15:34 +0530

    Url Shortner completed in caller.go

--- goLangProjects/urlShortner/caller.go
+++ goLangProjects/urlShortner/caller.go
@@ -3,6 +3,8 @@ package main
 import (
 	"fmt"
 	"net/http"
+
+	"github.com/tidwall/gjson"
 )
 
 func main() {
@@ -11,17 +13,23 @@ func main() {
 	// Build the MapHandler using the mux as the fallback
 	pathsToUrls := map[string]string{
 		"/urlshortgophers": "https://godoc.org/github.com/gophercises/urlshort",
-		"/yaml-godoc":     "https://godoc.org/gopkg.in/yaml.v2",
-    "/mygit" : "https://github.com/yashwagle/goLibrary",
-
-  }
+		"/yaml-godoc":      "https://godoc.org/gopkg.in/yaml.v2",
+		"/mygit":           "https://github.com/yashwagle/goLibrary",
+	}
 	mapHandler := MapHandlerFunc(pathsToUrls, mux)
+
+	jsonPaths := `{
+	"/fb": "https://www.facebook.com/",
+	"/yt": "https://www.youtube.com/",
+	"/go-doc":"https://golang.org/doc/",
+	"/jsonlib":"https://github.com/tidwall/gjson"`
 
 	// Build the YAMLHandler using the mapHandler as the
 	// fallback
+	jsonHandler := JsonHandlerFunc(jsonPaths, mapHandler)
 
 	fmt.Println("Starting the server on :9999")
-	http.ListenAndServe(":9999", mapHandler)
+	http.ListenAndServe(":9999", jsonHandler)
 }
 
 func defaultMux() *http.ServeMux {
@@ -34,7 +42,6 @@ func hello(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintln(w, "Hello, world!")
 }
 
-
 func MapHandlerFunc(pathstoUrls map[string]string, fallback http.Handler) http.HandlerFunc {
 
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
@@ -43,6 +50,18 @@ func MapHandlerFunc(pathstoUrls map[string]string, fallback http.Handler) http.H
 			fallback.ServeHTTP(w, r)
 		} else {
 			http.Redirect(w, r, res, 301)
+		}
+	})
+}
+
+func JsonHandlerFunc(pathstoUrls string, fallback http.Handler) http.HandlerFunc {
+
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		res := gjson.Get(pathstoUrls, r.RequestURI)
+		if !res.Exists() {
+			fallback.ServeHTTP(w, r)
+		} else {
+			http.Redirect(w, r, res.String(), 301)
 		}
 	})
 }
