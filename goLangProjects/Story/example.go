59356a7ef4c8b4df4fadbcbcd0afd4fc9d08d9a7
Yash Wagle <yash27wagle@gmail.com>
2018-08-27 01:17:03 +0530

    added connectivity between frontend and backend

--- goLangProjects/Story/example.go
+++ goLangProjects/Story/example.go
@@ -9,15 +9,22 @@ import (
 func hello(w http.ResponseWriter, r *http.Request) {
 	http.ServeFile(w, r, "frontend.html")
 
-	//	var t = []byte("hello")
-	//	w.Write(t)
+	var t = []byte("hello")
+	w.Write(t)
+
+}
+func start(w http.ResponseWriter, r *http.Request) {
+
+	var t = []byte("hello")
+
+	w.Write(t)
 
 }
 
 func main() {
 	http.HandleFunc("/", hello)
 	http.Handle("/scripts/", http.StripPrefix("/scripts/", http.FileServer(http.Dir("scripts"))))
-
+	http.HandleFunc("/start", start)
 	fmt.Printf("Starting server for testing HTTP POST...\n")
 	if err := http.ListenAndServe(":9999", nil); err != nil {
 		log.Fatal(err)
